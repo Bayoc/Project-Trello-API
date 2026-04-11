faa8cde0c89aba8d03c05812e248c922f37e76f7
Szorstki <49475765+Bayoc@users.noreply.github.com>
2026-04-11 09:12:59 +0200

    Adds edge case test with max character limit and +1

--- tests/boards/create-board.spec.ts
+++ tests/boards/create-board.spec.ts
@@ -7,7 +7,7 @@ import { createBoardData } from '../../data/board.data';
 test.describe('CREATE Board', () => {
 
     test.describe('Positive Scenarios', () => {
-    let boardID: string = '';
+        let boardID: string = '';
         test.afterEach(async ({ request }) => {
             // cleanup - delete board
             const response = await request.delete(ENDPOINTS.BOARD.BY_ID(boardID), {
@@ -28,6 +28,21 @@ test.describe('CREATE Board', () => {
 
             boardID = body.id;
         });
+
+        test('POST Create Board with characters limit in name - should return 200 and create board', async ({ request }) => {
+            const longName = 'a'.repeat(16384);
+            const response = await request.post(ENDPOINTS.BOARD.BASE, {
+                params: { key: ENV.api_key, token: ENV.token },
+                data: { name: longName }
+            })
+
+            expect(response.status()).toBe(200);
+            const body = await response.json();
+            expect(body).toHaveProperty('id');
+            expect(body.name).toBe(longName);
+
+            boardID = body.id;
+        });
     });
 
     test.describe('Negative Scenarios', () => {
@@ -40,5 +55,17 @@ test.describe('CREATE Board', () => {
             const body = await response.json();
             expect(body.message).toBe('invalid value for name');
         });
+
+        test('POST Create Board with characters limit +1 in name - should return 400 error', async ({ request }) => {
+            const response = await request.post(ENDPOINTS.BOARD.BASE, {
+                params: { key: ENV.api_key, token: ENV.token },
+                data: { name: 'a'.repeat(16385) }
+            })
+
+            expect(response.status()).toBe(400);
+            const body = await response.json();
+            expect(body.message).toBe('invalid value for name');
+        });
+
     });
 });
