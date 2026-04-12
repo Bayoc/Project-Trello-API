415113b5f026f92eb0cd1444651e4e975a222a76
Szorstki <49475765+Bayoc@users.noreply.github.com>
2026-04-12 09:21:08 +0200

    Adds update-boards PUT tests

---
+++ tests/boards/update-board.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test';
+import { ENV } from '../../playwright.config';
+import { ENDPOINTS } from '../../data/endpoints';
+import { createBoardData } from '../../data/board.data';
+
+
+test.describe('PUT Board', () => {
+
+    let boardID: string = '';
+
+    test.beforeAll(async ({ request }) => {
+        // create BOARD
+        const response = await request.post(ENDPOINTS.BOARD.BASE, {
+            params: { key: ENV.api_key, token: ENV.token },
+            data: createBoardData
+        })
+        const body = await response.json();
+        boardID = body.id;
+    });
+    test.afterAll(async ({ request }) => {
+        // cleanup - delete board
+        const response = await request.delete(ENDPOINTS.BOARD.BY_ID(boardID), {
+            params: { key: ENV.api_key, token: ENV.token },
+        });
+    });
+
+    test.describe('Positive Scenarios', () => {
+        test('PUT Update Board Name - board name should be updated', async ({ request }) => {
+            const response = await request.put(ENDPOINTS.BOARD.BY_ID(boardID), {
+                params: { key: ENV.api_key, token: ENV.token, name: 'Updated Name' },
+            })
+
+            expect(response.status()).toBe(200);
+            const body = await response.json();
+            expect(body.name).toBe('Updated Name');
+
+        });
+    });
+
+    test.describe('Negative Scenarios', () => {
+        test('PUT Update Board with invalid ID - should return 404 not found', async ({ request }) => {
+            const response = await request.put(ENDPOINTS.BOARD.BY_ID('aaaaaaaaaaaaaaaaaaaaaaaa'), {
+                params: { key: ENV.api_key, token: ENV.token },
+            })
+            expect(response.status()).toBe(404);
+        });
+    });
+});
